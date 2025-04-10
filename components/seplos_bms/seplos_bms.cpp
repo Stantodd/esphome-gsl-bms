985ee329473b69f319651dd5d061b6cb94597516
Stantodd <162260420+Stantodd@users.noreply.github.com>
2025-04-10 09:24:46 -0400

    Update seplos_bms.cpp

--- components/seplos_bms/seplos_bms.cpp
+++ components/seplos_bms/seplos_bms.cpp
@@ -123,6 +123,10 @@ void SeplosBms::on_telemetry_data_(const std::vector<uint8_t> &data) {
   //  this->publish_state_(this->state_of_charge_sensor_, (float) seplos_get_16bit(offset + 9) * 0.1f);
   //   67     0x00 0x46      Number of cycles                 70
   this->publish_state_(this->charging_cycles_sensor_, (float) seplos_get_16bit(offset + 13));
+  
+  if (data.size() < offset + 15 + 2) {
+    return;
+  }
 
   //   65     0x46 0x50      Rated capacity                   18000 * 0.01f = 180.00        Ah
   this->publish_state_(this->rated_capacity_sensor_, (float) seplos_get_16bit(offset + 11) * 0.01f);
@@ -132,10 +136,13 @@ void SeplosBms::on_telemetry_data_(const std::vector<uint8_t> &data) {
   }
 
   //   67     0x00 0x46      Number of cycles                 70
-  this->publish_state_(this->charging_cycles_sensor_, (float) seplos_get_16bit(offset + 13));
+  // this->publish_state_(this->charging_cycles_sensor_, (float) seplos_get_16bit(offset + 13));
 
-  if (data.size() < offset + 15 + 2) {
-    return;
+  //  if (data.size() < offset + 15 + 2) {
+  //  return;
+
+  //   63     0x03 0x13      Stage of charge                  787 * 0.1f = 78.7             %
+  this->publish_state_(this->state_of_charge_sensor_, (float) seplos_get_16bit(offset + 9) * 0.1f);
   }
 
   //   69     0x03 0xE8      State of health                  1000 * 0.1f = 100.0           %
